782768d2d9ef94e936f6d9147110492020977160
SublimeDevv <juanmen1404@gmail.com>
2023-10-06 09:27:37 -0500

    Se agrega catalogo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Dashboard from './Views/Dashboard'
 import Error404 from './Error404';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Catalogue from './Views/Catalogue';
 function App() {
 
 
@@ -12,6 +13,7 @@ function App() {
     <Routes>
       <Route path="/Dashboard" element={<Dashboard/>}/>
       <Route path="/*" element={<Error404/>}/>
+      <Route path="/catalogue" element={<Catalogue/>}/>
     </Routes>
     </BrowserRouter>
     </>

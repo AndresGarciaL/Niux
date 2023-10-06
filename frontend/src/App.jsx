522c0cb1595f0637b87d0172aea67d616d1c553e
AndresGarciaL <andresgarciia09@gmail.com>
2023-10-06 12:37:37 -0600

    Se agrega View Home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Dashboard from './Views/Dashboard'
 import Error404 from './Error404';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Contacto from './Views/Contacto';
+import Home from './Views/Home';
 
 function App() {
 
@@ -12,10 +13,10 @@ function App() {
     
     <BrowserRouter>
     <Routes>
-      <Route path="/Dashboard" element={<Dashboard/>}/>
+      <Route path="/home" element={<Home/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
       <Route path="/Contacto" element={<Contacto/>}/>
       <Route path="/Error" element={<Error404/>}/>
-
     </Routes>
     </BrowserRouter>
     </>
---
+++ frontend/src/Views/Home.jsx
@@ -0,0 +1,13 @@
+import Breadcrums from "./Breadcrums";
+import Navbar from "./Navbar";
+
+function Home (){
+    return(
+        <>
+        <Navbar/>
+        <Breadcrums/>   
+           
+        </>
+    )
+}
+export default Home;

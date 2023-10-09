e525359b532cd610cdf68a7aff09085b4f3addfc
AndresGarciaL <andresgarciia09@gmail.com>
2023-10-09 11:12:05 -0600

    Se elimina BannerHome.jsx

--- frontend/src/Views/Home.jsx
+++ frontend/src/Views/Home.jsx
@@ -1,15 +1,11 @@
 import Breadcrums from "./Breadcrums";
 import Navbar from "./Navbar";
-import Banner_Home from "../Components/Banner_Home";
 
 function Home (){
     return(
         <>
         <Navbar/>
-        <Breadcrums/>  
-        <Banner_Home/>  
-
-           
+        <Breadcrums/>           
         </>
     )
 }

c499396459337442b1b490e9b8497b6c63c6be82
Angel Guzman <angelguzman10911@gmail.com>
2023-11-22 03:27:49 -0500

    se mejora servicios

--- frontend/src/Components/Dashboard/Tickets/FormTickets.jsx
+++ frontend/src/Components/Dashboard/Tickets/FormTickets.jsx
@@ -6,13 +6,52 @@ import { Label } from 'flowbite-react';
 import { Select, initTE } from "tw-elements";
 import "tw-elements-react/dist/css/tw-elements-react.min.css";
 import DynamicSelect from './Selects_CrearTickets/DynamicSelect';
-
+import { niuxApi } from '../../../api/niuxApi';
 
 
 initTE({ Select });
 
 
 const FormTickets = () => {
+
+  // Estado para almacenar los datos del formulario
+  const [formData, setFormData] = useState({
+    applicant: '',
+    title: '',
+    description: '',
+    type: '',
+    category: '',
+  });
+
+
+  // Manejador para cambios en los inputs
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  // Función para enviar los datos del formulario
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+    try {
+      const response = await niuxApi.post('/tickets', formData);
+      console.log('Ticket creado con éxito:', response.data);
+      // Restablecer el formulario
+      setFormData({
+        applicant: '',
+    title: '',
+    description: '',
+    type: '',
+    category: '',
+      });
+    } catch (error) {
+      console.error('Error al crear el ticket:', error);
+    }
+  };
   const statusOptions = [
     <span><RiTicketLine /> Nuevo</span>,
     <span><RiTicketLine /> En proceso</span>,
@@ -24,6 +63,8 @@ const FormTickets = () => {
   const [showTickets, setshowTickets] = useState(true);
   const [showParticipants, setshowParticipants] = useState(false);
   return (
+    
+    <form onSubmit={handleSubmit}  >
     <div className=''>
       <div className='bg-gray-200 borde rounded-md'>
       <button 
@@ -36,7 +77,10 @@ const FormTickets = () => {
   <RiArrowRightSLine className={`mt-1 ${showTickets && 'rotate-90'} transition-all`} />
 </button>
 
+
       {/* INICIO FORM */}
+      
+
       <div className='bg-gray-100'>
       <ul className={` ${showTickets ? 'h[700px]' : 'h-0'} overflow-y-hidden transition-all`}>
         {/* FECHA TICKET */}
@@ -46,12 +90,30 @@ const FormTickets = () => {
             <DatePickers />
           </div>
         </li>
+        <select name="tipo" value={formData.tipo} onChange={handleChange} className='w-full mb-10'>
+          <option value="">Seleccione Tipo</option>
+          <option value="Solicitud">Solicitud</option>
+          <option value="Incidencia">Incidencia</option>
+        </select>
+        <select name="tipo" value={formData.tipo} onChange={handleChange} className='w-full mb-10'>
+          <option value="">Seleccione Categoria</option>
+          <option value="Solicitud">Solicitud</option>
+          <option value="Incidencia">Incidencia</option>
+        </select>
 
-        <DynamicSelect label="Tipo" size={"2"} options={['Solicitud','Incidencia']} />
-        <DynamicSelect label="Categoría" size={"5"} options={['Servicio soporte remoto', 'Servicio Mantenimiento correctivo PC', 'Venta Memoria RAM', 'Servicio Mantenimiento CCTV','Servicio Mantenimiento CCTV', 'Servicio Mantenimiento CCTV']} />
+        <select name="tipo" value={formData.tipo} onChange={handleChange} className='w-full mb-10'>
+          <option value="">Seleccione Ubicación</option>
+          <option value="Niux">Niux</option>
+          <option value="Firenow Solutions">Firenow Solutions</option>
+          <option value="Publico en General">Publico en General</option>
+          <option value="Mexleasing">Mexleasing</option>
+          <option value="Hotel Villas del Palmar">Hotel Villas del Palmar</option>
+
+        </select>
+       {/*  <DynamicSelect label="Categoría" size={"5"} options={['Servicio soporte remoto', 'Servicio Mantenimiento correctivo PC', 'Venta Memoria RAM', 'Servicio Mantenimiento CCTV','Servicio Mantenimiento CCTV', 'Servicio Mantenimiento CCTV']} />
         <DynamicSelect label="Estado" size={"5"} options={statusOptions} />
         <DynamicSelect label="Ubicación" size={"5"} options={['Niux','Firenow Solutions', 'Publico en General', 'Mexleasing', 'Hotel Villas del Palmar', 'Mexleasing Queretaro']} />
-
+ */}
       </ul>
       </div>
       </div>
@@ -71,14 +133,24 @@ const FormTickets = () => {
       <div className='bg-gray-100'>
       <ul className={` ${showParticipants ? 'h[700px]' : 'h-0'} overflow-y-hidden transition-all`}>
 
-      <DynamicSelect label="Solicitante" size={"5"} options={['Fidencio Garcia Lopez', 'Omar Caballero', 'Mirian Poot Poot','Juan Diego Mendoza', 'Lorena Hernandez']} />
-      <DynamicSelect label="Observador" size={"5"} options={['Fidencio Garcia Lopez', 'Omar Caballero', 'Mirian Poot Poot','Juan Diego Mendoza', 'Lorena Hernandez']} />
-      <DynamicSelect label="Técnico" size={"5"} options={['Fidencio Garcia Lopez', 'Omar Caballero', 'Mirian Poot Poot','Juan Diego Mendoza', 'Lorena Hernandez']} />     </ul>
+      <select name="tipo" value={formData.tipo} onChange={handleChange} className='w-full mb-10 mt-8 h-7 border-[2px]'>
+          <option value="">Seleccione Tecnico</option>
+          <option value="Fidencio Garcia Lopez">Fidencio Garcia Lopez</option>
+          <option value="Omar Caballero">Omar Caballero</option>
+          <option value="Mirian Poot Poot">Mirian Poot Poot</option>
+          <option value="Juan Diego Mendoza">Mexleasing</option>
+          <option value="Lorena Hernandez">Lorena Hernandez</option>
+        </select>
+
+       <DynamicSelect label="Solicitante" size={"5"} options={['Fidencio Garcia Lopez', 'Omar Caballero', 'Mirian Poot Poot','Juan Diego Mendoza', 'Lorena Hernandez']} />
+     {/* <DynamicSelect label="Técnico" size={"5"} options={['Fidencio Garcia Lopez', 'Omar Caballero', 'Mirian Poot Poot','Juan Diego Mendoza', 'Lorena Hernandez']} />  */}    </ul>
       </div>
       </div>
      
      
     </div>
+     </form>
+    
   );
 };
 
--- frontend/src/Views/Home.jsx
+++ frontend/src/Views/Home.jsx
@@ -4,11 +4,12 @@ import Carrusel from "../Components/Home/Carrusel";
 import "../Styles/Home.css";
 import ProductosNuevos from "../Components/Home/ProductosNuevos";
 import OfertasEspeciales from "../Components/Home/OfertasEspeciales";
-import Principal from "../../public/Images/img_principal_home.jpg";
+import Principal from "../../public/Images/pru9.png";
 import { Link } from "react-router-dom";
 import Background from "../Components/Home/Background";
 import { ScrollMarcas } from "../Components/Home/ScrollMarcas";
 import { CountOnUs } from "../Components/Home/CountOnUs";
+import { Footer } from "./footer";
 
 
 function Home() {
@@ -20,14 +21,14 @@ function Home() {
       <Navbar />
       <Breadcrums />
       <div className=" relative ">
-        <img src={Principal} className=" h-[400px]  w-full" />
-        <h1 className=" absolute text-white inset-0 flex justify-center items-center text-7xl font-bold z-20">
+        <img src={Principal} className=" h-[500px]  w-full yes" />
+        <h1 className=" absolute text-white inset-0 flex justify-center items-center text-7xl font-bold z-20 tracking-widest">
           NIUX
         </h1>
         <div className="absolute inset-0 text-white  flex justify-center items-end z-20">
           <Link
             to="/catalogue"
-            className="  h-[30px] w-[150px] bg-purple-600 flex justify-center mb-20 rounded-lg items-center transition ease-in duration-100 hover:scale-105"
+            className="  h-[30px] w-[150px] bg-purple-600 flex justify-center mb-20 rounded-lg items-center transition ease-in duration-100 hover:scale-105 border "
           >
             Comprar Ahora
           </Link>
@@ -44,6 +45,8 @@ function Home() {
       <OfertasEspeciales />
       <CountOnUs/>
       <ScrollMarcas/>
+      <Footer/>
+
       
       
             </div>
